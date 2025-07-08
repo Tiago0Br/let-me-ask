2ebb69053b44733a8a9e7fbb07a349b2dd201717
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-07-08 18:25:36 -0300

    feature: criação da rota de listagem de salas (rooms)
    - Foi criada a rota para listagem das salas
    - Foi criado um arquivo .env.example para servir de exemplo
    - Foi criado o arquivo client.http para armazenar requests de exemplo das rotas criadas

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,8 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,6 +17,8 @@ app.register(fastifyCors, {
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-app.listen({ port: 3333 }).then(() => {
+app.register(getRoomsRoute)
+
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server running')
 })
