0893dec0b25e8267a1ed9b3aae765c54e8f9839a
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-07-11 20:32:02 -0300

    feature: adicionando paginação na listagem de salas

--- server/src/http/routes/get-rooms.ts
+++ server/src/http/routes/get-rooms.ts
@@ -1,24 +1,42 @@
-import { count, eq } from 'drizzle-orm'
+import { count, desc, eq } from 'drizzle-orm'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod/v4'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
 
 export const getRoomsRoute: FastifyPluginAsyncZod = async (app) => {
-  app.get('/rooms', async () => {
-    const result = await db
-      .select({
-        id: schema.rooms.id,
-        name: schema.rooms.name,
-        questionsCount: count(schema.questions.id),
-        createdAt: schema.rooms.createdAt,
-      })
-      .from(schema.rooms)
-      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
-      .groupBy(schema.rooms.id, schema.rooms.name)
-      .orderBy(schema.rooms.createdAt)
+  app.get(
+    '/rooms',
+    {
+      schema: {
+        querystring: z.object({
+          limit: z.coerce.number().default(5),
+          offset: z.coerce.number().default(0),
+        }),
+      },
+    },
+    async (request) => {
+      const { limit, offset } = request.query
 
-    return {
-      result,
+      console.log(limit, offset)
+
+      const result = await db
+        .select({
+          id: schema.rooms.id,
+          name: schema.rooms.name,
+          questionsCount: count(schema.questions.id),
+          createdAt: schema.rooms.createdAt,
+        })
+        .from(schema.rooms)
+        .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+        .groupBy(schema.rooms.id, schema.rooms.name)
+        .orderBy(desc(schema.rooms.createdAt))
+        .limit(limit)
+        .offset(offset)
+
+      return {
+        result,
+      }
     }
-  })
+  )
 }
